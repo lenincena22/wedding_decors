96c4c8af58cc7359b4529bba304e421eb306cf74
Balakumar <balakumar062001@gmail.com>
2022-08-03 09:52:21 +0530

    header commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import './sass/main.scss'
 import Navbar from './components/navbar/Navbar';
+import Header from './layouts/header/Header';
+
 
 function App() {
   return (
     <div >
       <Navbar/>
-      
+      <Header/>
     </div>
   );
 }
